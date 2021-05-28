48a6a66bd482d73ca34cf754437f792de9b4c159
Amit Edelshtein <amit@camilyo.com>
2021-05-28 15:53:10 -0400

    Initial commit

---
+++ WebApplication/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+// ReSharper disable UnusedMember.Global
+#pragma warning disable 1998
+
+namespace WebApplication
+{
+    public class MyClass
+    {
+        public async Task DoSomething() { }
+
+        public void DoSomethingElse() { }
+
+        public void Do_AnotherThing() { }
+
+        public void Do_Another_Other_Thing() { }
+    }
+}
